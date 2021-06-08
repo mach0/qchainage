3ff53c08d877d68c5f161193e3983ddf7c997bcb
Werner Macho <werner.macho@gmail.com>
2021-06-08 21:50:49 +0200

    delete artefakt

--- chainagetool.py
+++ chainagetool.py
@@ -18,15 +18,6 @@ Date      	By	Comments
 ----------	---	---------------------------------------------------------
 '''
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
 
 from qgis.PyQt.QtCore import (
     QVariant
